07c4f02117913b35ffe14ff4e151ab57d1cc82a8
annette <annettejohn31@gmail.com>
2021-01-25 23:33:52 +0530

    changed port number

--- app.js
+++ app.js
@@ -165,5 +165,5 @@ app.delete('/blogs/:id', (req, res)=> {
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log("Server has started"))
